9e9c3e8279bc51ec16e2f0a872cb43857b8a35f8
Natanaël RALAIVOAVY <143139607+NathanRael@users.noreply.github.com>
2025-01-21 21:07:09 +0300

    chore : create useRedirect hook

---
+++ src/hook/useRedirect.js
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+export const useRedirect = () => {
+    const history = useNavigate(); 
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const seeAnyway = urlParams.get('seeAnyway');
+
+        if (seeAnyway !== 'true') {
+            window.location.href = 'https://nathanrael.vercel.app/';
+        }
+    }, []);
+}
